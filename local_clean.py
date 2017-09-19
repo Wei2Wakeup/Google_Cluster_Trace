8b17d58ed8d477d11758ed910da50e7cdda4bb50
Chirs <wanwei@umich.edu>
2017-09-18 22:40:01 -0700

    Update local_clean.py

--- local_clean.py
+++ local_clean.py
@@ -1,24 +1,24 @@
 import  pandas as pd
 import numpy as np
 
-# daily_count=pd.DataFrame(np.zeros(29,dtype=np.int64),columns=['Req Num'])
-# day_len = 1e6*60*60*24
-# for i in range(0,25):
-#         filename='agrregate'+str(i)+'.csv'
-#         tmp= pd.read_csv(filename)
-#         tmp=tmp[tmp['Event Type']==0]
-#         first=tmp.iloc[0]['Time']
-#         first_day = np.floor((first-600*1e6)/day_len).astype(int)
-#         last=tmp.iloc[tmp.shape[0]-1]['Time']
-#         last_day=np.ceil((last-600*1e6)/day_len).astype(int)
-#
-#         for day in range(first_day,last_day):
-#             start = 600*1e6+day*day_len
-#             end = 600*1e6+(day+1)*day_len
-#             tmp_count = tmp[(tmp['Time']>=start) & (tmp['Time']<end)].shape[0]
-#             daily_count.iloc[day]['Req Num']=daily_count.iloc[day]['Req Num']+tmp_count
-# print(daily_count)
-# daily_count.to_csv('daily_count.csv',index=False)
+daily_count=pd.DataFrame(np.zeros(29,dtype=np.int64),columns=['Req Num'])
+day_len = 1e6*60*60*24
+for i in range(0,25):
+        filename='agrregate'+str(i)+'.csv'
+        tmp= pd.read_csv(filename)
+        tmp=tmp[tmp['Event Type']==0]
+        first=tmp.iloc[0]['Time']
+        first_day = np.floor((first-600*1e6)/day_len).astype(int)
+        last=tmp.iloc[tmp.shape[0]-1]['Time']
+        last_day=np.ceil((last-600*1e6)/day_len).astype(int)
+
+        for day in range(first_day,last_day):
+            start = 600*1e6+day*day_len
+            end = 600*1e6+(day+1)*day_len
+            tmp_count = tmp[(tmp['Time']>=start) & (tmp['Time']<end)].shape[0]
+            daily_count.iloc[day]['Req Num']=daily_count.iloc[day]['Req Num']+tmp_count
+print(daily_count)
+daily_count.to_csv('daily_count.csv',index=False)
 
 
 # daily_kill=pd.DataFrame(np.zeros(29,dtype=np.int64),columns=['Req Num'])
@@ -182,10 +182,10 @@ import numpy as np
 # print(count2)
 
 
-filename = 'agrregate' + str(0) + '.csv'
-tmp = pd.read_csv(filename)
-while(True):
-    finish = tmp[(tmp['Event Type'] >= 2) & (tmp['Event Type'] <= 6)].iloc[0]
+# filename = 'agrregate' + str(0) + '.csv'
+# tmp = pd.read_csv(filename)
+# while(True):
+#     finish = tmp[(tmp['Event Type'] >= 2) & (tmp['Event Type'] <= 6)].iloc[0]
 
 
 
